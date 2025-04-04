f131a59fd48b9124bee348a57edf13012fd377aa
tomokisun <github@tomokisun.com>
2025-04-05 04:08:13 +0900

    feat: increment visitors count

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,13 @@
 import { createRoute } from 'honox/factory'
 import HomePage from '../components/pages/HomePage'
+import { Context, Env } from 'hono';
 
 export default createRoute(async (c) => {
+  await incrementVisitorsCount(c);
+  return c.render(<HomePage c={c} />)
+})
+
+async function incrementVisitorsCount(c: Context<Env, any, {}>) {
   let visitorsCount = await c.env.KV.get('VISITORS_COUNT');
   if (visitorsCount) {
     const next = Number(visitorsCount) + 1;
@@ -11,6 +17,5 @@ export default createRoute(async (c) => {
     visitorsCount = '1';
     await c.env.KV.put('VISITORS_COUNT', visitorsCount);
   }
-
-  return c.render(<HomePage c={c} />)
-})
+  return visitorsCount;
+}
