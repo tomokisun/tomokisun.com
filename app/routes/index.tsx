d2686a4a213289a0ef07f1b1e23646baca087cb7
tomokisun <github@tomokisun.com>
2025-04-05 04:05:53 +0900

    feat: visitors count

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,5 +2,15 @@ import { createRoute } from 'honox/factory'
 import HomePage from '../components/pages/HomePage'
 
 export default createRoute(async (c) => {
+  let visitorsCount = await c.env.KV.get('VISITORS_COUNT');
+  if (visitorsCount) {
+    const next = Number(visitorsCount) + 1;
+    await c.env.KV.put('VISITORS_COUNT', next.toString());
+    visitorsCount = next.toString();
+  } else {
+    visitorsCount = '1';
+    await c.env.KV.put('VISITORS_COUNT', visitorsCount);
+  }
+
   return c.render(<HomePage c={c} />)
 })
--- app/server.ts
+++ app/server.ts
@@ -6,19 +6,6 @@ const app = createApp()
 
 app.use(logger());
 
-app.use('/', async (c, next) => {
-  let visitorsCount = await c.env.KV.get('VISITORS_COUNT');
-  if (visitorsCount) {
-    const next = Number(visitorsCount) + 1;
-    await c.env.KV.put('VISITORS_COUNT', next.toString());
-    visitorsCount = next.toString();
-  } else {
-    visitorsCount = '1';
-    await c.env.KV.put('VISITORS_COUNT', visitorsCount);
-  }
-  await next();
-})
-
 showRoutes(app)
 
 export default app
