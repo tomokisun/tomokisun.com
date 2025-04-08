7882d2b985ab46e095fb49d9825dd80984b08c20
tomokisun <github@tomokisun.com>
2025-04-09 00:43:42 +0900

    feat: add meta

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -13,6 +13,14 @@ export default jsxRenderer(({ children }) => {
         <title>tomokisun's homepage</title>
         <meta name="description" content="Welcome to tomokisun's personal homepage! Best viewed in Netscape Navigator 4.0 or Internet Explorer 5.0." />
         <meta name="keywords" content="tomokisun, personal, homepage, web, mobile, iOS, engineer, 90s, retro" />
+        <meta name="author" content="tomokisun" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="tomokisun's homepage" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/ogp.png" />
+        <meta property="og:url" content="https://tomokisun.com" />
+        <meta property="og:description" content="Welcome to tomokisun's personal homepage! Best viewed in Netscape Navigator 4.0 or Internet Explorer 5.0." />
+        <meta property='twitter:card' content='summary_large_image' />
       </head>
       <body>
         {children}
